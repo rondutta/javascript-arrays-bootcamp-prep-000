5dbaa5a92b7ec4ae4d3a6173311e339e201844d7
Rayan Dutta <rondutta23@gmail.com>
2018-05-15 14:55:23 +0000

    automatically backed up by learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -1,5 +1,5 @@
 /*global describe, it */
-
+/*
 const expect = require('chai').expect
 const fs = require('fs')
 const jsdom = require('mocha-jsdom')
@@ -128,3 +128,4 @@ describe('arrays', () => {
     })
   })
 })
+*/
